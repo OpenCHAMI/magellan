affba7dfa30068f5f948f551255c2a48b850c71a
David Allen <allend@lanl.gov>
2024-09-19 16:38:46 -0600

    Added CheckUntil() for tests

---
+++ internal/util/util.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"fmt"
+	"time"
+)
+
+// CheckUntil regularly check a predicate until it's true or time out is reached.
+func CheckUntil(interval time.Duration, timeout time.Duration, predicate func() (bool, error)) error {
+	timeoutCh := time.After(timeout)
+
+	for {
+		select {
+		case <-time.After(interval):
+			predTrue, err := predicate()
+			if predTrue {
+				return nil
+			}
+
+			if err != nil {
+				return err
+			}
+		case <-timeoutCh:
+			return fmt.Errorf("timeout of %ds reached", int64(timeout/time.Second))
+		}
+	}
+}
