e529e0fda323c576a52b635926df1bbc89e79a06
David J. Allen <davidallendj@gmail.com>
2024-11-03 21:25:52 -0700

    updated references in cmd/cache.go

--- cmd/cache.go
+++ cmd/cache.go
@@ -6,8 +6,8 @@ import (
 	"os"
 	"strconv"
 
-	magellan "github.com/OpenCHAMI/magellan/internal"
-	"github.com/OpenCHAMI/magellan/internal/cache/sqlite"
+	magellan "github.com/davidallendj/magellan/internal"
+	"github.com/davidallendj/magellan/internal/cache/sqlite"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
