ad0708d2adbbb7a7cb7291607482da9e2faea186
Devon Bautista <devonb@lanl.gov>
2025-04-16 16:29:20 -0600

    refactor: split BMC data structures into pkg/bmc package

---
+++ pkg/bmc/bmc.go
@@ -0,0 +1,45 @@
+package bmc
+
+import (
+	"encoding/json"
+
+	"github.com/OpenCHAMI/magellan/pkg/secrets"
+	"github.com/rs/zerolog/log"
+)
+
+type BMCCredentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func GetBMCCredentials(store secrets.SecretStore, id string) (BMCCredentials, error) {
+	var creds BMCCredentials
+	if uriCreds, err := store.GetSecretByID(id); err != nil {
+		// Specific credentials for URI not found, fetch default.
+		log.Warn().Str("id", id).Msg("specific credentials not found, falling back to default")
+		defaultSecret, err := store.GetSecretByID(secrets.DEFAULT_KEY)
+		if err != nil {
+			// We've exhausted all options, the credentials will be blank unless
+			// overridden by a CLI flag.
+			log.Warn().Str("id", id).Err(err).Msg("no default credentials were set, they will be blank unless overridden by CLI flags")
+		} else {
+			// Default credentials found, use them.
+			if err = json.Unmarshal([]byte(defaultSecret), &creds); err != nil {
+				log.Warn().Str("id", id).Err(err).Msg("failed to unmarshal default secrets store credentials")
+				return creds, err
+			} else {
+				log.Info().Str("id", id).Msg("default credentials found, using")
+			}
+		}
+	} else {
+		// Specific URI credentials found, use them.
+		if err = json.Unmarshal([]byte(uriCreds), &creds); err != nil {
+			log.Warn().Str("id", id).Err(err).Msg("failed to unmarshal specific credentials")
+			return creds, err
+		} else {
+			log.Info().Str("id", id).Msg("specific credentials found, using")
+		}
+	}
+
+	return creds, nil
+}
--- pkg/crawler/main.go
+++ pkg/crawler/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/OpenCHAMI/magellan/pkg/bmc"
 	"github.com/OpenCHAMI/magellan/pkg/secrets"
 	"github.com/rs/zerolog/log"
 	"github.com/stmcginnis/gofish"
@@ -18,13 +19,8 @@ type CrawlerConfig struct {
 	UseDefault      bool
 }
 
-func (cc *CrawlerConfig) GetUserPass() (BMCUsernamePassword, error) {
+func (cc *CrawlerConfig) GetUserPass() (bmc.BMCCredentials, error) {
 	return loadBMCCreds(*cc)
-}
-
-type BMCUsernamePassword struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
 }
 
 type EthernetInterface struct {
@@ -373,10 +369,10 @@ func walkManagers(rf_managers []*redfish.Manager, baseURI string) ([]Manager, er
 	return managers, nil
 }
 
-func loadBMCCreds(config CrawlerConfig) (BMCUsernamePassword, error) {
+func loadBMCCreds(config CrawlerConfig) (bmc.BMCCredentials, error) {
 	// NOTE: it is possible for the SecretStore to be nil, so we need a check
 	if config.CredentialStore == nil {
-		return BMCUsernamePassword{}, fmt.Errorf("credential store is invalid")
+		return bmc.BMCCredentials{}, fmt.Errorf("credential store is invalid")
 	}
 	creds, err := config.CredentialStore.GetSecretByID(config.URI)
 	if err != nil {
@@ -391,19 +387,19 @@ func loadBMCCreds(config CrawlerConfig) (BMCUsernamePassword, error) {
 				event := log.Error()
 				event.Err(err)
 				event.Msg("failed to get default credentials from secret store")
-				return BMCUsernamePassword{}, err
+				return bmc.BMCCredentials{}, err
 			}
 		} else {
-			return BMCUsernamePassword{}, err
+			return bmc.BMCCredentials{}, err
 		}
 	}
-	var bmc_creds BMCUsernamePassword
+	var bmc_creds bmc.BMCCredentials
 	err = json.Unmarshal([]byte(creds), &bmc_creds)
 	if err != nil {
 		event := log.Error()
 		event.Err(err)
 		event.Msg("failed to unmarshal credentials")
-		return BMCUsernamePassword{}, err
+		return bmc.BMCCredentials{}, err
 	}
 	return bmc_creds, nil
 }
