004fc7f91557b045f4c217c9fb9f67938426469f
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2025-07-03 10:48:01 -0600

    daemon: add username/password flags, support credential cache
    Signed-off-by: Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -5,10 +5,14 @@ import (
 	"time"
 
 	"github.com/OpenCHAMI/magellan/internal/cache/sqlite"
+	"github.com/OpenCHAMI/magellan/pkg/bmc"
+	"github.com/OpenCHAMI/magellan/pkg/crawler"
 	"github.com/OpenCHAMI/magellan/pkg/daemon"
+	"github.com/OpenCHAMI/magellan/pkg/secrets"
 
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/stmcginnis/gofish/redfish"
 )
 
@@ -25,6 +29,7 @@ var (
 var DaemonCmd = &cobra.Command{
 	Use: "daemon",
 	Example: `  magellan daemon
+  magellan daemon -i -u username -p password
   magellan daemon --print-only`,
 	Args:  cobra.ExactArgs(0),
 	Short: "Launch in daemon mode, listening for user commands and BMC events",
@@ -32,6 +37,28 @@ var DaemonCmd = &cobra.Command{
 		"Also listens for user commands, such as instructing a node to change its power state.\n" +
 		"Monitors nodes found via earlier scans; see the 'scan' command for information about performing a scan.",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Set up crawler config for BMC connections
+		var (
+			store      secrets.SecretStore
+			fetchCreds bool
+			err        error
+		)
+
+		if username != "" && password != "" {
+			fetchCreds = false
+			// First, try and load credentials from --username and --password if both are set.
+			log.Debug().Msgf("--username and --password specified, using them for BMC credentials")
+			store = secrets.NewStaticStore(username, password)
+		} else {
+			fetchCreds = true
+			// Alternatively, locate specific credentials (falling back to default) and override those
+			// with --username or --password if either are passed.
+			log.Debug().Msgf("one or both of --username and --password NOT passed, will attempt to obtain missing credentials from secret store at %s", secretsFile)
+			if store, err = secrets.OpenStore(secretsFile); err != nil {
+				log.Error().Err(err).Msg("failed to open local secrets store")
+			}
+		}
+
 		// Set appropriate output function
 		var do_output func(string, redfish.PowerSubsystem)
 		if printOnly {
@@ -53,8 +80,37 @@ var DaemonCmd = &cobra.Command{
 		for _, r := range scannedResults {
 			fmt.Printf("%s:%d (%s) @%s\n", r.Host, r.Port, r.Protocol, r.Timestamp.Format(time.UnixDate)) // FIXME:
 
+			store := store
+			uri := fmt.Sprintf("%s://%s", r.Protocol, r.Host)
+			fmt.Printf("DEBUG: Composed URI %s\n", uri)
+			if fetchCreds {
+				// Either none of the flags were passed or only one of them were; get
+				// credentials from secrets store to fill in the gaps.
+				bmcCreds, _ := bmc.GetBMCCredentials(store, uri)
+				nodeCreds := secrets.StaticStore{
+					Username: bmcCreds.Username,
+					Password: bmcCreds.Password,
+				}
+
+				// If either of the flags were passed, override the fetched
+				// credentials with them.
+				if username != "" {
+					log.Info().Msg("--username was set, overriding username for this BMC")
+					nodeCreds.Username = username
+				}
+				if password != "" {
+					log.Info().Msg("--password was set, overriding password for this BMC")
+					nodeCreds.Password = password
+				}
+
+				store = &nodeCreds
+			}
+
 			var config = crawler.CrawlerConfig{
-				// TODO: Build a crawler config for the current node
+				URI:             uri,
+				CredentialStore: store,
+				Insecure:        insecure,
+				UseDefault:      true,
 			}
 
 			err = daemon.CreateBMCPowerSubscription(config, daemon.Subscription{
@@ -79,6 +135,13 @@ var DaemonCmd = &cobra.Command{
 }
 
 func init() {
+	DaemonCmd.Flags().StringVarP(&username, "username", "u", "", "Set the username for the BMC")
+	DaemonCmd.Flags().StringVarP(&password, "password", "p", "", "Set the password for the BMC")
+	DaemonCmd.Flags().BoolVarP(&insecure, "insecure", "i", false, "Ignore SSL errors")
+	DaemonCmd.Flags().StringVarP(&secretsFile, "secrets-file", "f", "secrets.json", "Set path to the node secrets file")
 	DaemonCmd.Flags().BoolVar(&printOnly, "print-only", false, "Just print node status updates, instead of sending them to SMD")
+
+	checkBindFlagError(viper.BindPFlag("crawl.insecure", DaemonCmd.Flags().Lookup("insecure")))
+
 	rootCmd.AddCommand(DaemonCmd)
 }
