aadcce9ecf7d67353e48e54f4c4a63e757abcffd
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2025-09-10 16:37:48 -0600

    daemon: refine HTTP endpoint generation; properly execute nested commands
    Signed-off-by: Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -40,8 +39,15 @@ func RunServer(rootCmd *cobra.Command) error {
 // Add an endpoint for the given command, and repeat recursively for any subcommands
 func createCommandTree(router *chi.Mux, endpoint string, cmd *cobra.Command) {
 	endpoint = endpoint + "/" + cmd.Name()
-	router.Get(endpoint, createHelpHandler(cmd))
-	router.Post(endpoint, createCommandHandler(cmd))
+	// Create a shallow copy of the relevant Cobra command, and set its
+	// output destination to this HTTP request's ResponseWriter
+	// NOTE: Without this, two parallel HTTP requests could race to call
+	// SetOut(), with the winner being overridden by the loser. That would
+	// cause the winning request to receive no response, and the losing
+	// request to get the output from both command invocations
+	targetCmd := cmd
+	router.Get(endpoint, createHelpHandler(targetCmd))
+	router.Post(endpoint, createCommandHandler(targetCmd))
 	for _, childCmd := range cmd.Commands() {
 		if childCmd.Runnable() || childCmd.HasSubCommands() {
 			createCommandTree(router, endpoint, childCmd)
@@ -52,34 +58,24 @@ func createCommandTree(router *chi.Mux, endpoint string, cmd *cobra.Command) {
 // Create an HTTP request handler that displays help for the given command
 func createHelpHandler(cmd *cobra.Command) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Create a shallow copy of the relevant Cobra command, and set
-		// its output destination to this HTTP request's ResponseWriter
-		// NOTE: Without this, two parallel HTTP requests could race to
-		// call SetOut(), with the winner being overridden by the
-		// loser. That would cause the winning request to receive no
-		// response, and the losing request to get the output from both
-		// command invocations
-		targetCmd := cmd
-		targetCmd.SetOut(w)
-		_ = targetCmd.Help()
+		cmd.SetOut(w)
+		_ = cmd.Help()
 		// Help() always returns nil; not sure why the function signature includes an error
 	}
 }
 
 // Create an HTTP request handler that executes the given command
 func createCommandHandler(cmd *cobra.Command) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Info().Msgf("%s handler: %s from %s", cmd.Name(), r.Method, r.RemoteAddr)
+	// NOTE: Unset our shallow copy's parent command, so that it can
+	// execute on its own. Otherwise, the Execute() call later will
+	// traverse up the command tree
+	parent := cmd.Parent()
+	if parent != nil {
+		parent.RemoveCommand(cmd)
+	}
 
-		// Create a shallow copy of the relevant Cobra command, and set
-		// its output destination to this HTTP request's ResponseWriter
-		// NOTE: Without this, two parallel HTTP requests could race to
-		// call SetOut(), with the winner being overridden by the
-		// loser. That would cause the winning request to receive no
-		// response, and the losing request to get the output from both
-		// command invocations
-		targetCmd := cmd
-		targetCmd.SetOut(w)
+	return func(w http.ResponseWriter, r *http.Request) {
+		cmd.SetOut(w)
 
 		// Split out each body line as a separate argument
 		body, err := io.ReadAll(r.Body)
@@ -89,11 +85,10 @@ func createCommandHandler(cmd *cobra.Command) func(w http.ResponseWriter, r *htt
 		} else {
 			args = []string{}
 		}
-		targetCmd.SetArgs(args)
+		cmd.SetArgs(args)
 
 		// Run the actual command
-		// FIXME: Runs the root command no matter what
-		err = targetCmd.Execute()
+		err = cmd.Execute()
 		if err != nil {
 			// FIXME: Fails since the first call to Write() creates a 200 status
 			w.WriteHeader(http.StatusInternalServerError)
